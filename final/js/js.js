f58d7ca9ae712088837335b5ccb56636c8ffa3f2
Arti194 <arti19421942@gmail.com>
2019-12-13 18:28:21 +0200

    Work Commit

---
+++ final/js/js.js
@@ -0,0 +1,12 @@
+let coll=document.getElementsByClassName('ButtonList');
+for (let i=0; i<coll.length; i++){
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.display=='flex'){
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'flex';
+        }
+    })
+}
