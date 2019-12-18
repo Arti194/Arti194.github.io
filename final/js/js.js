c40f887b26663bf8eaa3d61de69b31d02c379722
Arti194 <arti19421942@gmail.com>
2019-12-18 02:42:06 +0200

    Work Commit

--- final/js/js.js
+++ final/js/js.js
@@ -1,12 +1,14 @@
-let coll=document.getElementsByClassName('ButtonList');
-for (let i=0; i<coll.length; i++){
-    coll[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-        let content = this.nextElementSibling;
-        if (content.style.display=='flex'){
-            content.style.display = 'none';
-        } else {
-            content.style.display = 'flex';
+const items =   document.querySelectorAll('.ListServices__item');
+for ( let i = 0; i < items.length; i++ ) {
+        items[i].onclick = function() {
+            if (this.classList.contains('ListServices__item-active')){
+                this.classList.remove('ListServices__item-active');
+            }
+            else{
+                for ( let i = 0; i < items.length; i++ ) {
+                    items[i].classList.remove('ListServices__item-active');
+                }
+                this.classList.toggle('ListServices__item-active');
+            }
         }
-    })
 }
